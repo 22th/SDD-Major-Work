d4bbce0c8f8acafe6a88e84a3addb8617a30bab4
22th <rsdelta343@gmail.com>
2018-03-26 17:19:59 +1100

    setup git ignore

--- Logic Puzzler.py
+++ Logic Puzzler.py
@@ -1,4 +1,9 @@
 import pygame as py
 import random as rand
 py.init()
-test
+#vars below
+screen_width = 1000
+screen_height = 700
+screen = pygame.display.set_mode((screen_width,screen_height))
+lock = pygame.time.Clock()
+myfont = pygame.font.SysFont("Comic Sans MS",15)
