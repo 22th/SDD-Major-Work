baedba362bee7e30222ae262a60cf34b59dd382a
22th <30763702+22th@users.noreply.github.com>
2018-03-27 16:33:46 +1100

    Updated thang and testing wing ide

--- Logic Puzzler.py
+++ Logic Puzzler.py
@@ -1,5 +1,6 @@
 import pygame as py
 import random as rand
+import gates
 py.init()
 #vars below
 screen_width = 1000
@@ -7,4 +8,11 @@ screen_height = 700
 screen = pygame.display.set_mode((screen_width,screen_height))
 lock = pygame.time.Clock()
 myfont = pygame.font.SysFont("Comic Sans MS",15)
-G="testing"
+running=True 
+
+while running == True:
+    lock.tick(100)
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT: 
+            running=False
