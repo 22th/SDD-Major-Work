bcc2f28a35bf863756c5f0d40c45db8b06a8e5f0
22th <30763702+22th@users.noreply.github.com>
2018-03-27 17:35:34 +1100

    Starting to functionalize the code

--- Logic Puzzler.py
+++ Logic Puzzler.py
@@ -12,29 +12,36 @@ myfont = py.font.SysFont("Comic Sans MS",15)
 running=True 
 gate=gates.LogicGates()
 txtbx = eztext.Input(maxlength=45, color=(255,255,255), prompt='type here: ')
+startbutt=py.image.load("startbutton.png")
+start=0
 #vars above
 def imgbutton(screen,img,x,y,):
     screen.blit(img,(x,y))
-    imgw=img.get_Width()
-    imgh=img.get_Height()
-    events = py.event.get()
-    for event in events:
-        if event.type == py.MOUSEBUTTONUP: 
+    imgw=img.get_width()
+    imgh=img.get_height()
+    ev = py.event.get()
+    for ev in events:
+        if ev.type == py.MOUSEBUTTONUP: 
             mousepos=py.mouse.get_pos()
             if x+imgw > mousepos[0] > x and y+imgh > mousepos[1] > y:
                 return(1)
+def main():
+    screen.fill((50,50,50))
+    start=imgbutton(screen,startbutt,350,100)
+    print(start)
+    txtbx.update(events)
+    #blit txtbx on the sceen
+    txtbx.set_pos(100,10)
+    txtbx.draw(screen)
+    py.display.flip()
+    
+            
 
 while running == True:
-    lock.tick(100)
+    lock.tick(10)
     events = py.event.get()
     for event in events:
         if event.type == py.QUIT: 
             running=False
-            
-            
-    #txtbx.update(events)
-    #blit txtbx on the sceen
-    #txtbx.set_pos(100,10)
-    #txtbx.draw(screen)
     
-    py.display.flip()
+    main()
