1ad38f95350e31cc2be2a67e81507dc37de58e8e
22th <30763702+22th@users.noreply.github.com>
2018-03-27 21:45:21 +1100

    Final Commit before bed

--- Logic Puzzler.py
+++ Logic Puzzler.py
@@ -14,6 +14,7 @@ gate=gates.LogicGates()
 txtbx = eztext.Input(maxlength=45, color=(255,255,255), prompt='Name: ')
 startbutt=py.image.load("startbutton.png")
 start=0
+SL=0
 #vars above
 def clckquit():
     lock.tick(100)
@@ -28,29 +29,48 @@ def imgbutton(screen,img,x,y,):
     imgh=img.get_height()
     ev = py.event.get()
     for ev in events:
-        if ev.type == py.MOUSEBUTTONUP: 
+        if ev.type == py.MOUSEBUTTONDOWN: 
             mousepos=py.mouse.get_pos()
             if x+imgw > mousepos[0] > x and y+imgh > mousepos[1] > y:
                 return(1)
 def Selectlevel():
-    clckquit()
+    lock.tick(10)
+    evets = py.event.get()
+    for event in evets:
+        if event.type == py.QUIT: 
+            running=False
+
     screen.fill((50,50,50))
     py.display.flip()
 def gamescreen(levelnum):
-    clckquit()
+    lock.tick(10)
+    events = py.event.get()
+    for event in events:
+        if event.type == py.QUIT: 
+            running=False
+
     screen.fill((50,50,50))
     py.display.flip()
 def main():
-    clckquit()
+    i=0
+    if i == 0:
+        S=0
     screen.fill((50,50,50))
     start=imgbutton(screen,startbutt,350,100)
     print(start)
     if start == 1:
-        Selectlevel()
-    txtbx.update(events)
+        S=1
+    #txtbx.update()
     #blit txtbx on the sceen
-    txtbx.set_pos(100,10)
-    txtbx.draw(screen)
+    #txtbx.set_pos(100,10)
+    #txtbx.draw(screen)
+    while S == 1:
+        events = py.event.get()
+        for event in events:
+            if event.type == py.QUIT: 
+                running=False
+        
+        Selectlevel()
     py.display.flip()
 while running == True: 
     lock.tick(10)
